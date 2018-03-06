7b6b70d168e9d11a6e76471263d16c2e2df22d70
shibo <tobias-e@gmx.net>
2018-03-06 11:09:20 +0100

    was alrdy right type just accessing was wrong

--- webserver/app.js
+++ webserver/app.js
@@ -4,43 +4,8 @@ var cors = require('cors');
 var app = express();
 app.use(express.static('public'));
 app.use(cors());
-var users2 = {
-    "users": [
-      {
-        "id": 1,
-        "age": 16,
-        "name": "Andy"
-      },
-      {
-        "id": 2,
-        "age": 22,
-        "name": "Lisa"
-      }
-    ],
-    "example2": [
-      {
-        "id": 1,
-        "date": "03-18-2016",
-        "message": "Nice!"
-      },
-      {
-        "id": 2,
-        "date": "06-21-2017",
-        "message": "Great view!"
-      }
-    ]
-  }
-
-  var users3 = {
-    "user":
-    {
-      "Name": "Shiro",
-      "Vorname": "Shibo",
-      "Alter": 42
-    }
-  }
-
-  var users = [
+var users = {
+  "users": [
     {
       "id": 1,
       "age": 16,
@@ -51,13 +16,48 @@ var users2 = {
       "age": 22,
       "name": "Lisa"
     }
+  ],
+  "example2": [
+    {
+      "id": 1,
+      "date": "03-18-2016",
+      "message": "Nice!"
+    },
+    {
+      "id": 2,
+      "date": "06-21-2017",
+      "message": "Great view!"
+    }
   ]
-  
+};
+
+/* var users3 = {
+  "user":
+    {
+      "Name": "Shiro",
+      "Vorname": "Shibo",
+      "Alter": 42
+    }
+}
+
+var users4 = [
+  {
+    "id": 1,
+    "age": 16,
+    "name": "Andy"
+  },
+  {
+    "id": 2,
+    "age": 22,
+    "name": "Lisa"
+  }
+] */
+
 
 app.get('/users', function (req, res) {
+  console.log(users.users);
+  res.send(users.users);
 
-  res.send(users);
-  console.log(users);
 });
 
 app.listen(3000, function () {
