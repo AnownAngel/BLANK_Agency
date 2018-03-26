39a260ea250a6789d099ff957dc59581d17f52ed
shibo <tobias-e@gmx.net>
2018-03-26 18:19:54 +0200

    test database for test section in angular added

--- webserver/app.js
+++ webserver/app.js
@@ -1,9 +1,11 @@
 
-var express = require('express');
-var app = express();
-var mysql = require('mysql');
-var bodyParser = require('body-parser');
-var cors = require('cors');
+var express = require('express'),
+app = express();
+mysql = require('mysql');
+bodyParser = require('body-parser');
+cors = require('cors');
+oauthserver = require('oauth2-server');
+
 
 
 app.use(bodyParser.json());
@@ -11,6 +13,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(cors());
 
+/* app.oauth = oauthserver({
+  model: require('./model'), 
+  grants: ['password'],
+  debug: true
+}); 
+
+app.all('/oauth/token', app.oauth.grant());*/
 
 
 var connection = mysql.createConnection({
@@ -23,11 +32,23 @@ var connection = mysql.createConnection({
 connection.connect();
 
 
+var con2 = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'users_test'
+});
+
+
+con2.connect();
 
 
 
 
-app.get('/hallo', function (req, res) {
+
+
+
+app.get('/hallo', /* app.oauth.authorise(), */ function (req, res) {
 
   connection.query('SELECT * FROM users', function (err, rows) {
     console.log(rows);
@@ -38,7 +59,7 @@ app.get('/hallo', function (req, res) {
 
 
 
-app.post('/lol', function (req, res) {
+app.post('/lol', /* app.oauth.authorise(), */ function (req, res) {
   var username = req.body.name;
   var usrname = JSON.stringify(username);
   connection.query('INSERT INTO users VALUES (0,' + usrname + ', "koks", "koks@koks.de", 27)', function (err, rows) {
@@ -50,8 +71,14 @@ app.post('/lol', function (req, res) {
 
 
 
+app.post('/test', /* app.oauth.authorise(), */ function (req, res) {
+  var test = req.body.name;
+  var test2 = JSON.stringify(test);
+  con2.query('INSERT INTO tbl_test VALUES (' + test2 + ')', function (err, rows) {
+    console.log(test2);
+  });
 
-
+});
 
 var users = {
   "users": [
@@ -108,6 +135,8 @@ app.get('/users', function (req, res) {
   res.send(users.users);
 
 });
+
+// app.use(app.oauth.errorHandler());
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
